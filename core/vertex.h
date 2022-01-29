c61464fd0572e4e56595e3f21bf0dfeb8fb7a846
SushiJackal <51118771+SushiJackal@users.noreply.github.com>
2022-01-29 02:13:30 +0100

    rewrote vertex processing with struct+offsets

---
+++ core/vertex.h
@@ -0,0 +1,8 @@
+//
+// Created by Bennet Weingartz on 28.01.22.
+//
+
+typedef struct{
+    float pos[4];
+    float col[4];
+} Vertex;
--- init/buffers.c
+++ init/buffers.c
@@ -4,45 +4,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <GL/glew.h>
 
 #include "buffers.h"
+#include "../core/vertex.h"
 
-GLuint vaoID, vboID, cboID;
+GLuint vaoID, vboID;
 
 void InitVBO(void){
-    GLenum errorValue = glGetError();
-    GLfloat vtxArray[] = {
-        0.3f, 0.7f, 0.0f, 1.0f,
-        0.7f, 0.7f, 0.0f, 1.0f,
-        0.5f, 0.3f, 0.0f, 1.0f,
-        -0.8f, -0.8f, 0.0f, 1.0f,
-        0.0f,  0.8f, 0.0f, 1.0f,
-        0.8f, -0.8f, 0.0f, 1.0f,
+    Vertex vtxArray[] = {
+    {{-0.8f, -0.8f,0.0f,1.0f }, {1.0f, 0.0f, 1.0f, 1.0f }},
+    {{ 0.0f,  0.8f,0.0f,1.0f }, {1.0f, 1.0f, 0.0f, 1.0f }},
+    {{ 0.8f, -0.8f,0.0f,1.0f }, {0.0f, 1.0f, 1.0f, 1.0f }},
+    {{ 0.3f,  0.7f,0.0f,1.0f }, {0.204f, 0.933f, 0.855f, 1.0f }},
+    {{ 0.5f,  0.3f,0.0f,1.0f }, {0.922f, 0.835f, 0.357f, 1.0f }},
+    {{ 0.7f,  0.7f,0.0f,1.0f }, {0.745f, 0.247f, 0.882f, 1.0f }}
     };
 
-    GLfloat colArray[] = {
-        1.0f, 0.0f, 0.0f, 1.0f,
-        0.0f, 1.0f, 0.0f, 1.0f,
-        0.0f, 0.0f, 1.0f, 1.0f,
-        1.0f, 0.0f, 0.0f, 1.0f,
-        0.0f, 1.0f, 0.0f, 1.0f,
-        0.0f, 0.0f, 1.0f, 1.0f
-    };
+    GLenum errorValue = glGetError();
+    const size_t bufSize = sizeof(vtxArray);
+    const size_t vtxSize = sizeof(vtxArray[0]);
+    const size_t rgbOffset = sizeof(vtxArray[0].pos);
 
     glGenVertexArrays(1, &vaoID); //Vertex Array Object
     glBindVertexArray(vaoID);
 
     glGenBuffers(1, &vboID); //Vertex Buffer Object
     glBindBuffer(GL_ARRAY_BUFFER, vboID);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vtxArray), vtxArray, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(0);
+    glBufferData(GL_ARRAY_BUFFER, bufSize, vtxArray, GL_STATIC_DRAW);
 
-    glGenBuffers(1, &cboID); //Color Buffer Object
-    glBindBuffer(GL_ARRAY_BUFFER, cboID);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colArray), colArray, GL_STATIC_DRAW);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, vtxSize, 0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, vtxSize, (GLvoid*) rgbOffset);
+
+    glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
     errorValue = glGetError();
@@ -62,11 +57,7 @@ void DestroyVBO(void) {
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glDeleteBuffers(1, &cboID);
     glDeleteBuffers(1, &vboID);
-
-    glBindVertexArray(0);
     glDeleteVertexArrays(1, &vaoID);
 
     errorValue = glGetError();
