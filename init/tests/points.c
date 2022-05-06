e0c1d1eaa0645c062deb3d9e51f3affb1a1c879e
SushiJackal <51118771+SushiJackal@users.noreply.github.com>
2022-05-06 18:13:39 +0200

    feat: velocity tracer on cursor (based on delta pos)

--- init/tests/points.c
+++ init/tests/points.c
@@ -20,9 +20,8 @@ Object** InitPoints(void){
     printf("Vertical: ");
     scanf("%i", &vt);
 
-    points = malloc(sizeof(Object) + hz * vt * sizeof(Vertex));
-    glm_mat4_identity(points->modelMatrix);
-    points->vertexCount = hz * vt;
+    points = malloc(sizeof(Object) + (hz * vt + 2) * sizeof(Vertex));
+    points->vertexCount = hz * vt + 2;
 
     for(int i = 0; i < vt; i++){
       for(int j = 0; j < hz; j++){
@@ -32,7 +31,14 @@ Object** InitPoints(void){
       }
     }
 
+    points->verts[points->vertexCount - 2] = (Vertex) {{0.0, 0.0, 0.5, 1.0}, {1.0, 1.0, 1.0, 1.0}};
+    points->verts[points->vertexCount - 1] = (Vertex) {{0.0, 0.0, 0.5, 1.0}, {1.0, 1.0, 1.0, 1.0}};
+
+    glm_mat4_inv(projectionMatrix, points->modelMatrix);
     glUniformMatrix4fv(modelMatrixUniformLocation, 1, GL_FALSE, (float*) points->modelMatrix);
+
+    MoveCamera(&camera, (vec3) {.0, .0, 2.0});
+
     glPointSize(3);
     printf("POINTS OK\n");
 
@@ -48,17 +54,27 @@ void DestroyPoints(void){
 
 void DrawPoints(void){
     double time = glfwGetTime();
-    if(time - deltaTime >= 0.05){
+    if(time - deltaTime >= 0.03){
       double x, y;
       glfwGetCursorPos(window, &x, &y);
       
       vec2 vel = {x - xPrev, y - yPrev};
-      printf("Cursor velocity: (%0.f, %0.f) px/s\n", *vel * 20, *(vel + 1) * 20);
+
+      points->verts[points->vertexCount - 2].pos[0] = 2.0 / currentWidth * xPrev - 1;
+      points->verts[points->vertexCount - 2].pos[1] = -2.0 / currentHeight * yPrev + 1;
+      points->verts[points->vertexCount - 1].pos[0] = 2.0 / currentWidth * x - 1;
+      points->verts[points->vertexCount - 1].pos[1] = -2.0 / currentHeight * y + 1;
+
+      RefreshVBO(points);
 
       deltaTime = time;
       xPrev = x;
       yPrev = y;
     }
 
-    glDrawArrays(GL_POINTS, 0, points->vertexCount);
+    glm_mat4_inv(projectionMatrix, points->modelMatrix);
+    glUniformMatrix4fv(modelMatrixUniformLocation, 1, GL_FALSE, (float*) points->modelMatrix);
+
+    glDrawArrays(GL_POINTS, 0, points->vertexCount - 2);
+    glDrawArrays(GL_LINES, points->vertexCount - 2, 2);
 }
