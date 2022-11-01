84ffab9f5ebb216f2294a60d71bbd7fcc20a8bb1
SushiJackal <weingaben@gmail.com>
2022-11-01 17:06:35 +0100

    init files

---
+++ physics/rays.c
@@ -0,0 +1,6 @@
+//
+// Created by Bennet Weingartz on 07.08.22.
+//
+
+#include "rays.h"
+
---
+++ physics/rays.h
@@ -0,0 +1,8 @@
+//
+// Created by Bennet Weingartz on 07.08.22.
+//
+
+#ifndef INDAGO_C_RAYS_H
+#define INDAGO_C_RAYS_H
+
+#endif //INDAGO_C_RAYS_H
