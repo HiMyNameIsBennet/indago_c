17737d9cc236800264ed50fc34b1a91a22c5f6f2
SushiJackal <weingaben@gmail.com>
2022-11-16 01:14:57 +0100

    implemented ray struct and initializer

--- physics/rays.c
+++ physics/rays.c
@@ -3,4 +3,31 @@
 //
 
 #include "rays.h"
+#include "../init/init.h"
+#include "../core/camera.h"
+
+
+Ray* InitRay(vec4 startNDC, vec4 endNDC){ //initializes world space ray from NDC
+	
+	//this function can be made more concise if operations like
+	//glm_mat4_inv(m1, m1) or glm_vec4_scale(v1, scale, v1)
+	//are allowed. check for this in debugger with some test cases
+	
+	Ray* ray = malloc(sizeof(Ray));
+
+	mat4 combinedMatrix, inverseMatrix;
+	glm_mat4_mul(projectionMatrix, viewMatrix, combinedMatrix);
+	glm_mat4_inv(combinedMatrix, inverseMatrix);
+
+	//turning NDC coordinates into world coordinates (NDC -> cam space -> world space)
+	vec4 startUnscaled, endUnscaled;
+	glm_mat4_mulv(inverseMatrix, startNDC, startUnscaled);
+	glm_mat4_mulv(inverseMatrix, endNDC, endUnscaled);
+
+	//normalizing vectors by w coordinate
+	glm_vec4_scale(startUnscaled, 1.0f / ray->start[3], ray->start);
+	glm_vec4_scale(endUnscaled, 1.0f / ray->end[3], ray->end);
+
+	return ray;
+}
 
--- physics/rays.h
+++ physics/rays.h
@@ -6,3 +6,16 @@
 #define INDAGO_C_RAYS_H
 
 #endif //INDAGO_C_RAYS_H
+
+
+#include <cglm/cglm.h>
+
+
+typedef struct {
+	vec4 start;
+	vec4 end;
+} Ray;
+
+
+Ray* InitRay(vec4 startNDC, vec4 endNDC);
+
